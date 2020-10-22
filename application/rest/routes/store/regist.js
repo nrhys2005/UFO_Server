f86ecf42951548ad33c4f17178dbc93c83f50801
nrhys2005 <56465527+nrhys2005@users.noreply.github.com>
2020-10-22 17:09:12 +0900

    store image

--- application/rest/routes/store/regist.js
+++ application/rest/routes/store/regist.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const models = require('../../models');
+const fs = require('fs');
 //store regist
 router.post('/', (req, res) =>{
+    image_path = "./img_store"+req.body.name
     models.Store.create({
         name : req.body.name,
-        img_url: req.body.img_url,
+        img_url: image_path,
         desc: req.body.desc,
         latitude : req.body.latitude,
         longitude: req.body.longitude
     }).then((result) => {
+        fs.mkdirSync(image_path, { recursive: true })
+        
         console.log(result);
         res.send(true);
     }).catch((err)=>{
