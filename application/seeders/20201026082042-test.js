43cb5ec7aa3e3e1f833203f43234f324976cfa30
cozy-ho <kj2693119@gmail.com>
2020-10-29 17:36:25 +0900

    [Del] fabric codes.

---
+++ application/seeders/20201026082042-test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   let datas = [];
+   let obj = {
+      email: "test@test.com",
+      name: "testUser",
+      password: "1234",
+      createdAt: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
+      updatedAt: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
+    }
+    datas.push(obj)
+
+    return queryInterface.bulkInsert('User', datas, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
