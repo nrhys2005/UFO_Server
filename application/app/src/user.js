d3b3a7ec8a13c917a912bc95d0d4ccbac1c91d8c
nrhys2005 <56465527+nrhys2005@users.noreply.github.com>
2020-11-25 21:31:15 +0900

    Update user.js

--- application/app/src/user.js
+++ application/app/src/user.js
@@ -1,6 +1,7 @@
-const crypto = require('crypto');
+const bcrypt = require('bcrypt');
 const models = require('../../database')
 
+//유저 확인
 exports.signin = (req, res) => {
     /*
     id = req.body.id
@@ -13,23 +14,23 @@ exports.signin = (req, res) => {
     */
     models.User.findOne({
         where: {
-            email : req.body.email,
-            pw : req.body.pw
+            kakao_id : req.body.kakao_id    
         }
     }).then((result) => {
-        console.log(result);
+        console.log('login : ' + kakao_id);
         res.send(true);
     }).catch((err) => {
         console.log(err);
         res.send(false);
     });
 }
-
+//유저등록(미완)
 exports.signup = (req, res) => {
+    //cutomer
+    var customer_id = "custmer"  //customer_id 안겹치는 문자열로 넣어야함.
     models.User.create({
-        email: req.body.email,
-        pw: req.body.pw,
-        name: req.body.name
+        kakao_id: req.body.kakao_id,
+        customer_id: customer_id
     }).then((result) => {
         console.log(result);
         res.send(true);
@@ -38,7 +39,65 @@ exports.signup = (req, res) => {
         res.send(false);
     });
 }
+//결제pw 등록(미완)
+exports.regist_pw = (req, res) => {
+    const saltRounds = 12
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        if(err) return res.send(false)
+        bcrypt.hash(req.body.pw, salt, function(err, hash) {
+            // Store hash in your password DB.
+            if(err) return res.send(false);
+            models.User.update({
+                pw : hash
+            },{
+                where: { kakao_id: req.body.kakao_id } 
+            }).then((result) => {
+                console.log(result);
+                res.send(true);
+            }).catch((error) => {
+                console.log(error);
+                res.send(false);
+            });
+        });
+    });  
+}
+//결제pw 확인(미완)
+exports.check_pw = (req, res) => {
+    models.User.findOne({
+        attributes: ['pw']
+    },{
+        where: {
+            kakao_id : req.body.kakao_id    
+        }
+    }).then((result) => {
+        bcrpt.compare(req.pw,result.pw)
+        .then(result => {
+            if(result) res.send(true)
+            else res.send(false)
+        })
+    }).catch((err) => {
+        console.log(err);
+        res.send(false);
+    });
+}
 
+//sales 인증(미완)
+exports.regist_sales = (req, res) => {
+    var sales_id = "sales"  //customer_id 안겹치는 문자열로 넣어야함.
+    models.User.update({
+        sales_id : sales_id
+    },{
+        where: { kakao_id: req.body.kakao_id }
+    }).then((result) => {
+        console.log(result);
+        res.send(true);
+    }).catch((error) => {
+        console.log(error);
+        res.send(false);    
+    });
+}
+
+//check 현재는 안씀
 exports.check = (req, res) => {
     models.User.findOne({
         where: { email: req.params.email }
