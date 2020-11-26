1136ab30b96c77f3b944ac18e632d14a168044ca
cozy-ho <kj2693119@gmail.com>
2020-11-26 17:50:57 +0900

    [Mod] menu API

--- application/app/routes/wallet.js
+++ application/app/routes/wallet.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const wallet = require('../src/wallet')
 
-module.exports = router
 
 router.post('/chargeMoney ', wallet.chargeMoney )
 router.post('/transferMoney ', wallet.transferMoney )
 router.post('/getWallet ', wallet.getWallet )
+
+module.exports = router
