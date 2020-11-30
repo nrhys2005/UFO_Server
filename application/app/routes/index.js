5cc103b798266e7cc7277972b317ee1b49bb5dd3
nrhys2005 <56465527+nrhys2005@users.noreply.github.com>
2020-11-30 16:24:15 +0900

    Update index.js

--- application/app/routes/index.js
+++ application/app/routes/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
 var session = require('express-session');
+var redis = require('./redis.js');
 
 router.use(session({
-    secret: 'session_secret!',
-    resave: false,
-    saveUninitialized: true,
+    tore: new RedisStore({
+        client: redis,
+        host: 'localhost',
+        port: 6379,
+        prefix : "session:",
+        db : 0,
+        saveUninitialized: false,
+        resave: false
+
+    }),
+    secret : 'UFO',
+    cookie : { maxAge: 2592000000 }
 }));
 //routing
 // router.use('/user', require('./user/index.js'));
@@ -16,5 +26,5 @@ router.use('/user', require('./user'));
 router.use('/wallet', require('./wallet'));
 router.use('/festival', require('./festival'));
 
-
+module.exports = redisClient;
 module.exports = router;
